5812ad603b7ed73e94fbbbbc831cdf01f8a8f1cc
Tiffany Cuneo <tiffanycuneo@gmail.com>
2019-02-01 14:38:16 -0700

    pushing map

---
+++ client/src/components/Map/index.js
@@ -0,0 +1,21 @@
+import {Map, InfoWindow, Marker} from 'google-maps-react';
+import GoogleApiWrapper from "../map/component/utils/google";
+
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
